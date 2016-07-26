21202cca63b677c673f30156595e3eef0bf9fbbc
lipei <pei@ilanever.com>
2016-07-27 00:01:57 +0800

    create config file.

---
+++ orbit-web/src/main/java/com/orbit/configs/SystemConfig.java
@@ -0,0 +1,53 @@
+package com.orbit.configs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class SystemConfig {
+
+	private static Logger logger = LogManager.getLogger(SystemConfig.class);
+
+	private static Properties properties;
+
+	public static String getProperty(String key) {
+		if (properties == null) {
+			properties = new Properties();
+			try {
+				InputStream in = Thread.currentThread().getContextClassLoader()
+						.getResourceAsStream("system.properties");
+				properties.load(in);
+			} catch (IOException e) {
+				logger.debug(e.getMessage(), e);
+			}
+		}
+		return properties.getProperty(key);
+	}
+
+	public static String getDatabaseDriver(){
+		return getProperty("db.connect.driver");
+	}
+
+	public static String getDatabaseUrl(){
+		return getProperty("db.connect.url");
+	}
+
+	public static String getDatabaseUsername(){
+		return getProperty("db.connect.username");
+	}
+
+	public static String getDatabasePassword(){
+		return getProperty("db.connect.password");
+	}
+
+	public static Integer getSystemCommonListPageSize(){
+		return Integer.valueOf(getProperty("system.common.list.pageSize"));
+	}
+
+	public static String getSharedfilesDir(){
+		return getProperty("sharedfiles.dir");
+	}
+}
