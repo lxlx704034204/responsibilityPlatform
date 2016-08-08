afbc86ed46a24813b26c973b2580616848cac5fd
qianyong <qianyong@jd.com>
2016-08-08 20:49:10 +0800

    增加自动化安装mysql-cluster，配置网卡，按照顺序启动mysql-cluster服务的脚本

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -16,19 +16,47 @@ sync_dir=/home/vagrant/sync
 rpm -ivh ${sync_dir}/MySQL-Cluster-server-gpl-7.4.11-1.el7.x86_64.rpm
 
 host=`hostname`
+
+systemctl disable mysql
+systemctl stop mysql
+
 if [ "$host" == "sql" ]
     then
+        cp ${sync_dir}/ifcfg-eth1-sql /etc/sysconfig/network-scripts/ifcfg-eth1
+        service network restart
         rpm -ivh ${sync_dir}/MySQL-Cluster-client-gpl-7.4.11-1.el7.x86_64.rpm
         cp ${sync_dir}/my.cnf /etc/my.cnf
+
+        #启动sql节点
+        /usr/bin/mysqld_safe --datadir=/var/lib/mysql --user=mysql --ndbcluster --ndb-connectstring=192.168.101.202:1186&
+
+        set -x
+
+        #重置root密码，并创建orbit库
+        # 获取mysql安装过程中随机生成的root账户密码
+        pass=`awk -F\): '{gsub(/^[ \t]+/, "", $2); gsub(/[ \t]+$/, "", $2); print $2}'  /root/.mysql_secret`
+        mysqladmin -uroot -p$pass password mysql
+
+        set +x
 elif [[ $host =~ ^data ]]
     then
+        cp ${sync_dir}/ifcfg-eth1-$host /etc/sysconfig/network-scripts/ifcfg-eth1
+        service network restart
         cp ${sync_dir}/my.cnf /etc/my.cnf
         mkdir -p /usr/local/mysql/data
         chown -R mysql:mysql /var/lib/mysql
+        #启动ndbd节点
+        ndbd
 elif [ "$host" == "manager" ]
     then
+        cp ${sync_dir}/ifcfg-eth1-manager /etc/sysconfig/network-scripts/ifcfg-eth1
+        service network restart
         mkdir -p /var/lib/mysql-cluster
         cp ${sync_dir}/config.ini /var/lib/mysql-cluster/
+        # 启动ndb_mgmd进程
+        ndb_mgmd -f /var/lib/mysql-cluster/config.ini
 else
     echo "error: should not be here"
 fi
+
+
